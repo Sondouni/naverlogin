559375b7ed1710cc3f8c5f7739e2d45e78d24df6
Sondouni <ezicland@gmail.com>
2022-01-29 12:35:23 +0900

    2022-01-22

--- src/main/java/com/whatshouldeat/study/searchImg/ImgChannel.java
+++ src/main/java/com/whatshouldeat/study/searchImg/ImgChannel.java
@@ -2,11 +2,13 @@ package com.whatshouldeat.study.searchImg;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class ImgChannel {
     private String lastBuildDate;
     private int total;
